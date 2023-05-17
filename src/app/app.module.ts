cb4c622e2f72a0506b32e5212b250536de920438
Sebastian Gomez <ssgomez@trabajo.gob.ar>
2023-05-17 00:04:43 -0300

    agregar app-curso-add al module index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursoListComponent } from './components/curso-list/curso-list.component';
 import { PerfilListComponent } from './components/perfil-list/perfil-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ModalModule } from 'ngx-bootstrap/modal';
 import { CursoAddComponent } from './components/curso-add/curso-add.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,7 +20,8 @@ import { CursoAddComponent } from './components/curso-add/curso-add.component';
     BrowserModule,
     HttpClientModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
