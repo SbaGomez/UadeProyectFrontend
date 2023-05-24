ebfa621e8cd9bfaf6d73793c6a3e553ae1889a02
Sebastian Gomez <ssgomez@trabajo.gob.ar>
2023-05-24 01:48:15 -0300

    cambios en curso add

--- src/app/components/curso-add/curso-add.component.ts
+++ src/app/components/curso-add/curso-add.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Curso } from 'src/app/models/curso';
 import { CursoService } from 'src/app/services/curso.service';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-curso-add',
@@ -11,39 +11,79 @@ import { CursoService } from 'src/app/services/curso.service';
 })
 export class CursoAddComponent {
 
-  curso = new Curso()
-  cursoForm: FormGroup
+  @ViewChild('modalAdd') modalAdd: any;
+  curso = new Curso();
+  cursoForm: FormGroup;
 
-  constructor(
-    private cursoservices: CursoService) { }
+  constructor(private cursoservices: CursoService, private modalService: NgbModal) { }
 
   ngOnInit() {
     this.curso.nombre = "";
     this.curso.duracion = 0;
     this.cursoForm = new FormGroup({
-      'nombre': new FormControl(this.curso.nombre, { validators: [Validators.required], updateOn: 'blur'}),
-      'duracion': new FormControl(this.curso.duracion, Validators.required)
-    })
+      nombre: new FormControl(this.curso.nombre, {
+        validators: [Validators.required],
+        updateOn: 'blur'
+      }),
+      duracion: new FormControl(this.curso.duracion || '', {
+        validators: [Validators.required],
+        updateOn: 'blur'
+      })
+    });
+    document.getElementsByTagName('input')[0].focus()
   }
 
-  get nombre() {return this.cursoForm.get('nombre')}
-  get duracion() {return this.cursoForm.get('duracion')}
+  get nombre() {
+    return this.cursoForm.get('nombre');
+  }
+  get duracion() {
+    return this.cursoForm.get('duracion');
+  }
+
+  public mensajeExito: string = '';
 
   addCurso() {
-    let curso = new Curso();
-    curso.nombre = this.curso.nombre
-    curso.duracion = this.curso.duracion;
-    this.cursoservices.add(curso).subscribe(() => {
-      this.curso.nombre = ""
-      this.curso.duracion = 0
-      console.log(curso);
-      location.reload()
-      document.getElementsByTagName("input")[0].focus()
-
-    }, error => {
-      console.error(error)
-      alert('Error: ' + error.error.message)
-      document.getElementsByTagName('input')[0].focus()
-    })
+    if (this.cursoForm.valid) {
+      const curso = new Curso();
+      curso.nombre = this.nombre.value;
+      curso.duracion = this.duracion.value;
+  
+      this.cursoservices.add(curso).subscribe(
+        () => {
+          this.nombre.setValue('');
+          this.duracion.setValue('');
+          document.getElementsByTagName('input')[0].focus();
+  
+          this.mensajeExito = `Curso ${curso.nombre} registrado`;
+          this.openModal(); // Abrir el modal de éxito
+        },
+        (error) => {
+          console.error(error);
+          if (error.error.text == 'Curso registrado') {
+            this.mensajeExito = `Curso ${curso.nombre} registrado`;
+            this.openModal(); // Abrir el modal de éxito
+          }
+          document.getElementsByTagName('input')[0].focus();
+        }
+      );
+    } else {
+      alert('Por favor, complete todos los campos obligatorios.');
+    }
   }
+  
+
+  openModal() {
+    this.modalService.open(this.modalAdd).result.then(
+      (result) => {
+        if (result === 'close') {
+          location.reload();
+        }
+      },
+      (reason) => {
+        // Manejar cualquier error o rechazo del modal si es necesario
+      }
+    );
+  }
+
+
 }
